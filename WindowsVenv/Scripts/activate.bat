347586036c60ac9ef35f62ad695800433006311c
NGminecraft <grahanic001@alpinesd.org>
2024-09-15 17:13:22 -0600

    Fixing the WindowsVenv it works :)

--- WindowsVenv/Scripts/activate.bat
+++ WindowsVenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\grahanic001\Documents\MixerCode\WindowsVenv
+set VIRTUAL_ENV=C:\Users\minec\OneDrive\Documents\MixerCode\WindowsVenv
 
 if not defined PROMPT set PROMPT=$P$G
 
