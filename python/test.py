eebe397c1f87642d8ac7923bcabcc14ecf31a01f
NGminecraft <grahanic001@alpinesd.org>
2024-09-16 13:02:50 -0600

    Deleting useless file

--- python/test.py
+++ python/test.py
@@ -1,19 +1,26 @@
 
 names = ["Jim", "Jane", "Joe", "Jill"]
 
-nums = sorted([2,5,3,7,9,10])
+nums = sorted([1,2,3,4,5,6,7,8,9,19])
 
-search_name = 9
+search_name = 8
 
-def binary_search(list, item):
+def binary_search(list, item, index = 0):
+    print(list, index)
     if len(list) > 2:
-        return binary_search(list[0:len(list)//2], item)
+        peek = list[len(list)//2]
+        if peek == item:
+            return peek, index+len(list)//2
+        elif peek < item:
+            return binary_search(list[len(list)//2+1:], item, index+len(list)//2)
+        elif peek > item:
+            return binary_search(list[0:len(list)//2], item, index)
     elif len(list) == 1 or list[0] == item:
-        return list[0]
+        return list[0], index
     elif list[1] == item:
-        return list[1]
+        return list[1], index+1
     else:
-        return None
+        return None, index
 
 def linear_search(list, item):
     for x in range(len(list)):
@@ -21,4 +28,5 @@ def linear_search(list, item):
             print(item, x)
             
             
-print(binary_search(nums))
+print(binary_search(nums, search_name))
+print(nums.index(search_name))
