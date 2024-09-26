4e2b037e5fab4fde1179a09dddab887a36db7e96
NGminecraft <grahanic001@alpinesd.org>
2024-09-25 22:20:31 -0600

    I don't know what I did. added asyncio stuff?

--- python/communication.py
+++ python/communication.py
@@ -1,24 +1,15 @@
 import logging
 from utilClasses.cmd import CMD
-<<<<<<< HEAD
-=======
-import socket
->>>>>>> c4d2f088005f40a683c0538e6f513bd420849fa7
 from pythonosc import dispatcher
 from pythonosc import osc_server, udp_client
 import asyncio
 from config import Config
-<<<<<<< HEAD
 import socket
-import asyncio
 
-=======
->>>>>>> c4d2f088005f40a683c0538e6f513bd420849fa7
 
 class Comms:
     def __init__(self, config:Config):
         self.logger = logging.getLogger("logger.main")
-<<<<<<< HEAD
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((config.local_ip, config.local_port))
@@ -34,7 +25,8 @@ class Comms:
                 sock=self.socket
             )
             return transport, protocol
-        self.logger.info(f"Starting server on {config.local_ip}:{config.local_port}, ({self.server.socket.getsockname()})")
+    
+        self.logger.info(f"Starting server on {config.local_ip}:{config.local_port}, ({self.socket.getsockname()})")
         
         self.logger.info("Started server, setting up client side")
         # This is setting up the sending of data (mixers data)
@@ -42,22 +34,10 @@ class Comms:
         # Even though you technically shouldn't do this, I'm desperate
         self.client._sock = self.socket
         self.logger.info(f"Set up client server on {self.client._sock.getsockname()}")
-        # asyncio get event loop run until ocmplete(create_server())
-        # loop.run_forever()
+        # starts the datagram, then kicks up the server
+        asyncio.get_event_loop().run_until_complete(create_server())
+        asyncio.get_event_loop().run_forever()
         
-=======
-        # This checks the config to check for the local devices ip and port for 
-        # This is for the part of the server responsible for receiving data (local devices data)
-        self.mainDispatcher = dispatcher.Dispatcher()
-        self.server = osc_server.AsyncIOOSCUDPServer((config.local_ip, config.local_port), self.mainDispatcher, asyncio.get_event_loop())
-        self.logger.info(f"Starting server on {config.local_ip}:{config.local_port}")
-        self.server.serve()
-        self.logger.info("Started server, setting up client side")
-        # This is setting up the sending of data (mixers data)
-        self.client = udp_client.SimpleUDPClient(config.remote_ip, config.remote_port)
-        # Even though you technically sholdn't do this, I'm desperate
-        self.client._sock.bind((config.local_ip, config.local_port))
->>>>>>> c4d2f088005f40a683c0538e6f513bd420849fa7
     
     def registerListener(self, func_object, cmd:CMD):
         if cmd:
@@ -70,7 +50,6 @@ class Comms:
         self.logger.debug(f"Sending message of {cmd} with value {value}")
         self.client.send_message(cmd, value)
             
-<<<<<<< HEAD
 """
 This I think is the type of socket we need to make
     shared_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -80,5 +59,3 @@ This I think is the type of socket we need to make
     
 We need to set the socket used in client._sock as well as the one in self.server.socket to this (hopefully)
 """
-=======
->>>>>>> c4d2f088005f40a683c0538e6f513bd420849fa7
